aa7f2fb5099074fd3de2a55f72942e179b24448a
Zafarwasay <abdulwasaythelegend@gmail.com>
2026-01-05 18:23:24 +0500

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,25 @@
+name = "Penguin"
+age = 15
+is_student = True
+weight = 38.5
+
+print ("Name:", name)
+print ("Datatype of Name is", type(name))
+
+print ("Age :", age)
+print ("Datatype of Age is", type(age))
+
+print ("Is_Student :", is_student)
+print ("Datatype of Is_Student is", type(is_student))
+
+print ("Weight :", weight)
+print ("Datatype of Weight is", type(weight))
+
+
+print ("\n after type Casting....")
+age = str(age)
+print(age)
+print("Datatype of age is", type(age))
+weight = int(weight)
+print(weight)
+print("Datatype of weight is", type(weight))
