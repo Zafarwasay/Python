dc09ca81237fe602fdfa034a0853798d7ad183ab
Zafarwasay <abdulwasaythelegend@gmail.com>
2026-01-07 18:02:11 +0500

    Add files via upload

---
+++ Odd even.py
@@ -0,0 +1,8 @@
+number = int(input("Enter number to check"))
+print("Number to check :", number)
+
+if number%2==0 :
+ print("Its an Even number")
+
+else:
+ print("Its an Odd number")
---
+++ calendar.py
@@ -0,0 +1,10 @@
+import datetime
+
+current_time = datetime.datetime.now()
+
+print("Time now at greenwich meridian : ", end = "")
+print(current_time)
+
+import calendar
+
+print(calendar.Calendar(2026))
